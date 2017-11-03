0f551ac55dfddde22d024108ffb1e6de35e088fb
Ale Lopez <alelopez.utnfrt@gmail.com>
2017-11-03 20:13:40 -0300

    -Added validation checks to article and section

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+  validates :headline, :lead, :body, presence: true
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,2 +1,4 @@
 class Section < ApplicationRecord
+  validates :name, presence: true
+
 end
