1c55d6f72102bf3fa943702200539139d6190f8e
Ale Lopez <alelopez.utnfrt@gmail.com>
2017-11-30 15:04:14 -0300

    [paperclip-to-article}
      -Added polymorphic association to article and banner with image

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,7 +1,7 @@
 class Banner < ApplicationRecord
   include EnumI18nHelper
 
-  has_one :image, dependent: :destroy
+  has_one :image, dependent: :destroy, as: :pictureable
 
   accepts_nested_attributes_for :image, allow_destroy: true
 
--- db/migrate/20171129210039_add_article_reference_to_image.rb
+++
@@ -1,5 +0,0 @@
-class AddArticleReferenceToImage < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :images, :article, foreign_key: true
-  end
-end
