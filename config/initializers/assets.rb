f57dd51f59696acf3955d77436f12d97051acdf7
Ale Lopez <alelopez.utnfrt@gmail.com>
2017-12-11 16:31:59 -0300

    [add-font-awesome]
      -Added font-awesome
      -Added landing-scss stylesheet for landing namespace
      -Removed section title glyphicon

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,5 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w(admin.scss editor.scss)
+Rails.application.config.assets.precompile += %w(admin.scss editor.scss landing.scss)
 Rails.application.config.assets.precompile += %w(admin.js editor.js)
