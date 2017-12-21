635a0ac09a6a3c9bde0af74a63023385b52c18dd
francisco <lamontanarofrancisco@gmail.com>
2017-12-21 13:08:52 -0300

    [fix-roboto-font] -add asset-url into style.scss -add font precompile to asset.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,5 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
 Rails.application.config.assets.precompile += %w(admin.scss editor.scss)
 Rails.application.config.assets.precompile += %w(admin.js editor.js)
