e279122a710d760ecab91a72c932108f528e2c7d
francisco <lamontanarofrancisco@gmail.com>
2017-12-28 17:00:10 -0300

    [fix-query-articles] -fix banner querys

--- app/controllers/landing/base_controller.rb
+++ app/controllers/landing/base_controller.rb
@@ -5,9 +5,14 @@ class Landing::BaseController < ApplicationController
 
   private
   def set_banners
-    @top_banner = Banner.by_position("top").last
-    @banner_right_first = Banner.by_position("right_first").last
-    @banner_right_second = Banner.by_position("right_second").last
-    @banner_right_third = Banner.by_position("right_third").last
+    @banner_all = Banner.all.to_a
+    @top_banner = find_banner(@banner_all, "top")
+    @banner_right_first = find_banner(@banner_all, "right_first")
+    @banner_right_second = find_banner(@banner_all, "right_second")
+    @banner_right_third = find_banner(@banner_all, "right_third")
+  end
+
+  def find_banner(banners, position)
+    banners.find { |banner| banner.position == position }
   end
 end
