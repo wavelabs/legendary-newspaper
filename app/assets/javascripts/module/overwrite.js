d51c47949c9544a511e79b577b4565fe5fabc352
francisco <alelopez.utnfrt@gmail.com>
2018-04-25 01:38:36 -0300

    Fix buttons social networks

--- app/assets/javascripts/module/overwrite.js
+++ app/assets/javascripts/module/overwrite.js
@@ -10,3 +10,11 @@ jQuery(document).ready(function($) {
 $(document).ready(function() {
   $().UItoTop({ easingType: 'easeOutQuart' });
 });
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.12&appId=198518799926&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
