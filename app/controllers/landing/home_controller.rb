498f8841037426678262e9d38cd8b09008b03089
Miguel <Miguel Carrizo>
2018-04-27 18:42:19 -0300

    [add-carrousel]
      -Fixed the size of the carousel
      -Added @carrousel_articles in the index in /controllers/landing/home_controller

--- app/controllers/landing/home_controller.rb
+++ app/controllers/landing/home_controller.rb
@@ -2,14 +2,11 @@ class Landing::HomeController < Landing::BaseController
   def index
     @decorated_articles = Article.approved.collect { |article| Landing::ArticlePresenter.new(article, view_context) }
     @sections = Section.with_articles
+    @carrousel_articles = Article.for_carrousel
   end
 
   def section
     @section = Section.friendly.find(params[:id])
     @section_articles = @section.articles.approved.collect { |article| Landing::ArticlePresenter.new(article, view_context) }
-  end
-
-  def carrousel
-    @decorated_articles = Article.for_carrousel
   end
 end
